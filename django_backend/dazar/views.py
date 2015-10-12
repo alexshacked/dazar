7f0b12e915e7fa76b224568dcd0ce66efe989747
Alex Shacked <al.shacked@gmail.com>
2015-10-12 11:10:10 +0300

    began working on the cellular API

--- django_backend/dazar/views.py
+++ django_backend/dazar/views.py
@@ -8,110 +8,130 @@ import re
 import urllib2
 import json
 
-def makeReturn(status, payload):
-    retblock = {}
-    retblock['status'] = status
-    if status == 'OK':
-        retblock['data'] = payload
-    else:
-        retblock['info'] = payload
+'The entry point to Dazar backend'
+class DazarAPI:
+    def register(self, request):
+        body = json.loads(request.body)
+        return HttpResponse(body['data'])
 
-    return retblock
+    def addTweet(self, request):
+        return HttpResponse('Hello from addTweet')
 
-def geocodeFromGoogle(civil_addr):
-    start = 'https://maps.googleapis.com/maps/api/geocode/json?address='
-    mid = civil_addr
-    end = '&key=AIzaSyAQ_Qt1ohwtRK84fy18fUpYllL0sZhX0wo'
-    uri = start + mid + end
+    def getTweets(self, request):
+        return HttpResponse('Hello from getTweets')
 
-    geocode = urllib2.urlopen(uri).read()
-    jsonObject = json.loads(geocode)
-    if jsonObject['status'] != 'OK':
-        return makeReturn('FAIL', 'google maps api - method geocode() failed')
+    # DEBUG API
+    def debugAddAddress(self, request):
+        initial_addr, formatted_addr = self._extractAddress(request)
+        geocode = self._geocodeFromGoogle(formatted_addr)
+        if geocode['status'] != 'OK':
+            return HttpResponse(json.dumps(geocode))
+        else:
+            geocode = geocode['data']
 
-    coords = jsonObject['results'][0]['geometry']['location']
-    return makeReturn('OK', coords)
+        pt = Point(type = 'Point', coordinates = [float(geocode['lng']), float(geocode['lat'])] )
+        try:
+            doc = Locations.objects.create(address=initial_addr, point=pt)
+            doc.save()
+        except Exception as e:
+            return HttpResponse(json.dumps(self._makeReturn('FAIL','Failed on access to MongoDb  ------- ' + e.message)))
 
-def extractAddress(request):
-    initial_addr = request.GET['addr']
-    formatted_addr = re.sub(r'[ ]+', '+', initial_addr)
-    return initial_addr, formatted_addr
+        return HttpResponse(json.dumps(self._makeReturn('OK','OK')))
 
-# API
-def addAddress(request):
-    initial_addr, formatted_addr = extractAddress(request)
-    geocode = geocodeFromGoogle(formatted_addr)
-    if geocode['status'] != 'OK':
-        return HttpResponse(json.dumps(geocode))
-    else:
-        geocode = geocode['data']
+    # DEBUG API
+    def debugNeighbours(self, request):
+        initial_addr, formatted_addr = self._extractAddress(request)
+        geocode = self._geocodeFromGoogle(formatted_addr)
+        if geocode['status'] != 'OK':
+            return HttpResponse(json.dumps(geocode))
+        else:
+            geocode = geocode['data']
 
-    pt = Point(type = 'Point', coordinates = [float(geocode['lng']), float(geocode['lat'])] )
-    try:
-        doc = Locations.objects.create(address=initial_addr, point=pt)
-        doc.save()
-    except Exception as e:
-        return HttpResponse(json.dumps(makeReturn('FAIL','Failed on access to MongoDb  ------- ' + e.message)))
+        maxDistance = int(request.GET['radius'])
+        lat = float(geocode['lat'])
+        lng = float(geocode['lng'])
+        query = { 'point': { '$near': {'$geometry': {'type':"Point", 'coordinates': [lng, lat]}, '$maxDistance': maxDistance } } }
 
-    return HttpResponse(json.dumps(makeReturn('OK','OK')))
+        try:
+            queryset = Locations.objects.raw_query(query)
+            n = len(queryset) # probably a Django MongoDB Engine issue. Empty queryset does not support the contract API
+        except Exception as e:
+            return HttpResponse(json.dumps(self._makeReturn('FAIL','Failed on access to MongoDb  ------- ' + e.message)))
 
-def parseMongoResponse(queryset):
-    res = []
-    for q in queryset:
-        store = {}
-        store['address'] = q.address
-        store['coordinates'] = {'latitude':q.point.coordinates[1], 'longitude':q.point.coordinates[0]}
-        res.append(store)
+        res = self._parseMongoResponse(queryset);
+        return HttpResponse(json.dumps(self._makeReturn('OK',res)))
 
-    return res
+    # DEBUG API
+    def debugAll(self, request):
+        try:
+            queryset = Locations.objects.all()
+            n = len(queryset) # probably a Django MongoDB Engine issue. Empty queryset does not support the contract API
+        except Exception as e:
+            return HttpResponse(json.dumps(self._makeReturn('FAIL','Failed on access to MongoDb  ------- ' + e.message)))
 
-# API
-def neighbours(request):
-    initial_addr, formatted_addr = extractAddress(request)
-    geocode = geocodeFromGoogle(formatted_addr)
-    if geocode['status'] != 'OK':
-        return HttpResponse(json.dumps(geocode))
-    else:
-        geocode = geocode['data']
+        res = self._parseMongoResponse(queryset);
+        return HttpResponse(json.dumps(self._makeReturn('OK',res)))
 
-    maxDistance = int(request.GET['radius'])
-    lat = float(geocode['lat'])
-    lng = float(geocode['lng'])
-    query = { 'point': { '$near': {'$geometry': {'type':"Point", 'coordinates': [lng, lat]}, '$maxDistance': maxDistance } } }
+    # DEBUG API
+    def debugTruncate(self, request):
+        try:
+            Locations.objects.all().delete()
+        except Exception as e:
+            return HttpResponse(json.dumps(self._makeReturn('FAIL','Failed on access to MongoDb  ------- ' + e.message)))
+        return HttpResponse(json.dumps(self._makeReturn('OK','OK')))
 
-    try:
-        queryset = Locations.objects.raw_query(query)
-        n = len(queryset) # probably a Django MongoDB Engine issue. Empty queryset does not support the contract API
-    except Exception as e:
-        return HttpResponse(json.dumps(makeReturn('FAIL','Failed on access to MongoDb  ------- ' + e.message)))
+    # DEBUG API
+    def debugGisUnittest(self, request):
+        t = get_template('gisUnittest.html')
+        html = t.render( Context({}) )
+        return HttpResponse(html)
 
-    res = parseMongoResponse(queryset);
-    return HttpResponse(json.dumps(makeReturn('OK',res)))
+    #DEBUG API
+    def debugApiConsole(self, request):
+        t = get_template('apiConsole.html')
+        html = t.render( Context({}) )
+        return HttpResponse(html)
 
-# API
-def all(request):
-    try:
-        queryset = Locations.objects.all()
-        n = len(queryset) # probably a Django MongoDB Engine issue. Empty queryset does not support the contract API
-    except Exception as e:
-        return HttpResponse(json.dumps(makeReturn('FAIL','Failed on access to MongoDb  ------- ' + e.message)))
+    def _makeReturn(self, status, payload):
+        retblock = {}
+        retblock['status'] = status
+        if status == 'OK':
+            retblock['data'] = payload
+        else:
+            retblock['info'] = payload
 
-    res = parseMongoResponse(queryset);
-    return HttpResponse(json.dumps(makeReturn('OK',res)))
+        return retblock
 
-# API
-def truncate(request):
-    try:
-        Locations.objects.all().delete()
-    except Exception as e:
-        return HttpResponse(json.dumps(makeReturn('FAIL','Failed on access to MongoDb  ------- ' + e.message)))
-    return HttpResponse(json.dumps(makeReturn('OK','OK')))
+    def _parseMongoResponse(self, queryset):
+        res = []
+        for q in queryset:
+            store = {}
+            store['address'] = q.address
+            store['coordinates'] = {'latitude':q.point.coordinates[1], 'longitude':q.point.coordinates[0]}
+            res.append(store)
 
-# API
-def gisUnittest(request):
-    t = get_template('gisUnittest.html')
-    html = t.render( Context({}) )
-    return HttpResponse(html)
+        return res
+
+    def _geocodeFromGoogle(self, civil_addr):
+        start = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+        mid = civil_addr
+        end = '&key=AIzaSyAQ_Qt1ohwtRK84fy18fUpYllL0sZhX0wo'
+        uri = start + mid + end
+
+        geocode = urllib2.urlopen(uri).read()
+        jsonObject = json.loads(geocode)
+        if jsonObject['status'] != 'OK':
+            return self._makeReturn('FAIL', 'google maps api - method geocode() failed')
+
+        coords = jsonObject['results'][0]['geometry']['location']
+        return self._makeReturn('OK', coords)
+
+    def _extractAddress(self, request):
+        initial_addr = request.GET['addr']
+        formatted_addr = re.sub(r'[ ]+', '+', initial_addr)
+        return initial_addr, formatted_addr
+
+
 
 
 
--- django_backend/django_backend/urls.py
+++ django_backend/django_backend/urls.py
@@ -1,19 +1,20 @@
 from django.conf.urls import patterns, include, url
-from dazar.views import addAddress
-from dazar.views import neighbours
-from dazar.views import all
-from dazar.views import truncate
-from dazar.views import gisUnittest
+from dazar.views import DazarAPI
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+api = DazarAPI()
 urlpatterns = patterns('',
-    url(r'^addAddress$', addAddress),
-    url(r'^neighbours$', neighbours),
-    url(r'^all$', all),
-    url(r'^truncate$', truncate),
-    url(r'^gisUnittest$', gisUnittest),
+    url(r'^register$', api.register),
+    url(r'^addTweet$', api.addTweet),
+    url(r'^getTweets$', api.getTweets),
+    url(r'^addAddress$', api.debugAddAddress),
+    url(r'^neighbours$', api.debugNeighbours),
+    url(r'^all$', api.debugAll),
+    url(r'^truncate$', api.debugTruncate),
+    url(r'^gisUnittest$', api.debugGisUnittest),
+    url(r'^apiConsole$', api.debugApiConsole),
 )
 
