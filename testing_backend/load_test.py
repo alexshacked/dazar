e4a39293ce6f503741b002ee76c98745a5872abd
Alex Shacked <al.shacked@gmail.com>
2015-10-18 00:47:57 +0300

    load test added

---
+++ testing_backend/load_test.py
@@ -0,0 +1,66 @@
+import urllib2
+import json
+import datetime
+
+
+class LoadTest:
+    def __init__(self):
+        self.BASE_URI = "http://dazar.io/"
+        self.REGISTER_URI = self.BASE_URI + 'registerVendor'
+        self.ADD_URI = self.BASE_URI + 'addTweet'
+        self.GET_URI = self.BASE_URI + 'getTweets'
+
+        self.log = open('./report.txt', 'w')
+
+        self.vendors = []
+
+    def doLog(self, msg):
+        now = datetime.datetime.now()
+        strNow = str(now)
+        final = strNow + ':   ' + msg + '\n'
+        self.log.write(final)
+        print final
+
+
+    def doRegisterVendor(self, vendor, address, phone, tags):
+        js = {"vendor": vendor,
+              "address": address,
+              "phone": phone,
+              "tags": tags}
+
+        flat = json.dumps(js)
+        req = urllib2.Request(url = self.REGISTER_URI, data = flat)
+        fromWire = urllib2.urlopen(req).read()
+        resp = json.loads(fromWire)
+        if resp['status'] == 'OK':
+            js['id'] = resp['data']['vendorId']
+
+        return resp, js
+
+    def start(self):
+        streets = [('ibn gvirol, tel aviv', 1, 200), ('dizengoff, tel aviv', 1, 200), ('hayarkon, tel aviv', 1, 300),
+                    ('derech namir, tel aviv', 1, 200), ('alenby, tel aviv', 1, 130)]
+
+        self.doLog('Start registration')
+        idx = 1
+        for street in streets:
+            self.doLog('Beginning to work on street ' + street[0])
+            for i in range(street[1], street[2]):
+                store = 'store ' + str(idx)
+                address = str(street[1]) + ' ' + str(i)
+                telephone = str(123456789 + idx)
+                tags = ['cafes']
+                idx = idx + 1
+                resp, js = self.doRegisterVendor(store, address, telephone, tags)
+                if resp['status'] == 'FAIL':
+                    self.doLog(resp['info'])
+                else:
+                    self.vendors.append(js)
+
+        self.doLog('End registration')
+
+
+
+
+tester = LoadTest()
+tester.start()
