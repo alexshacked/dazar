2d10945771987e25a2f70b34c1e5ffb8501b4f41
Alex Shacked <al.shacked@gmail.com>
2015-11-10 11:55:40 +0200

    added location services

--- ios/Dazar/Dazar/FirstViewController.swift
+++ ios/Dazar/Dazar/FirstViewController.swift
@@ -1,6 +1,109 @@
 import UIKit
+import CoreLocation
+import MapKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager?
+    
+    func locationManager(manager: CLLocationManager!,
+        didChangeAuthorizationStatus status: CLAuthorizationStatus){
+            
+            print("The authorization status of location services is changed to: ")
+            
+            switch CLLocationManager.authorizationStatus(){
+            case .Authorized:
+                print("Authorized")
+            case .AuthorizedWhenInUse:
+                print("Authorized when in use")
+            case .Denied:
+                print("Denied")
+            case .NotDetermined:
+                print("Not determined")
+            case .Restricted:
+                print("Restricted")
+            default:
+                print("Unhandled")
+            }
+            
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didFailWithError error: NSError!){
+            print("Location manager failed with error = \(error)")
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didUpdateToLocation newLocation: CLLocation!,
+        fromLocation oldLocation: CLLocation!){
+            
+            print("Latitude = \(newLocation.coordinate.latitude)")
+            print("Longitude = \(newLocation.coordinate.longitude)")
+            
+    }
+    
+    func displayAlertWithTitle(title: String, message: String){
+        let controller = UIAlertController(title: title,
+            message: message,
+            preferredStyle: .Alert)
+        
+        controller.addAction(UIAlertAction(title: "OK",
+            style: .Default,
+            handler: nil))
+        
+        presentViewController(controller, animated: true, completion: nil)
+    }
+
+    func createLocationManager(startImmediately: Bool){
+        locationManager = CLLocationManager()
+        if let manager = locationManager {
+            print("Successfully created the location manager")
+            manager.delegate = self
+            if startImmediately{
+                manager.startUpdatingLocation()
+            }
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        /* Are location services available on this device? */
+        if CLLocationManager.locationServicesEnabled() {
+            
+            /* Do we have authorization to access location services? */
+            switch CLLocationManager.authorizationStatus(){
+            case .Authorized:
+                /* Yes, always. */
+                createLocationManager(true)
+            case .AuthorizedWhenInUse:
+                /* Yes, only when our app is in use. */
+                createLocationManager(true)
+            case .Denied:
+                /* No. */
+                displayAlertWithTitle("Not Determined",
+                    message: "Location services are not allowed for this app")
+            case .NotDetermined:
+                /* We don't know yet; we have to ask */
+                createLocationManager(false)
+                if let manager = self.locationManager{
+                    manager.requestWhenInUseAuthorization()
+                }
+            case .Restricted:
+                /* Restrictions have been applied; we have no access
+                to location services. */
+                displayAlertWithTitle("Restricted",
+                    message: "Location services are not allowed for this app")
+            }
+            
+            
+        } else {
+            /* Location services are not enabled.
+            Take appropriate action: for instance, prompt the
+            user to enable the location services. */
+            print("Location services are not enabled")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
