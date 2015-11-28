fc5f21869388ad795e3f193d64e209e10e23d9ea
Alex Shacked <al.shacked@gmail.com>
2015-11-28 17:02:50 +0200

    new vendor categories added

--- ios/Dazar/Dazar/NewVendorController.swift
+++ ios/Dazar/Dazar/NewVendorController.swift
@@ -17,7 +17,62 @@ protocol LocationControllerDelegate: class {
 
 class NewVendorController: UITableViewController {
     
+    var items: [TagItem]
     //weak var delegate: LocationControllerDelegate?
+    
+    required init(coder aDecoder: NSCoder) {
+        items = [TagItem]()
+        items.append(TagItem(text: Tag.Restaurants.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Cafes.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Clothing.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Shoes.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Electronix.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Beauty.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Home.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Grocery.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Kids.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Media.simpleDescription(), checked: false))
+        items.append(TagItem(text: Tag.Sports.simpleDescription(), checked: false))
+        
+        super.init(coder: aDecoder)!
+    }
+    
+    func resetTags(arr: [String]) {
+        if arr[0] == "all" {
+            for item in items {
+                item.checked = true
+            }
+        } else {
+            for a in arr {
+                for item in items {
+                    if a == item.text {
+                        item.checked = true
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section != 2 {
+            return super.tableView(tableView, didSelectRowAtIndexPath: indexPath)
+        }
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            let item = items[indexPath.row]
+            item.toggle()
+            cellCheck(cell, item: item)
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func cellCheck(cell: UITableViewCell, item: TagItem) {
+        if item.checked {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
+    }
     
     @IBAction func cancel() {
         print("new vendor cancel")
@@ -27,7 +82,20 @@ class NewVendorController: UITableViewController {
     
     @IBAction func done() {
         print("location done")
-        dismissViewControllerAnimated(true, completion: nil)
+        
+        var tags = [String]()
+        for item in items {
+            if item.checked {
+                tags.append(item.text)
+            }
+        }
+        if tags.isEmpty {
+            displayAlertWithTitle("Input data missing",
+                message: "Must provide business name, phone, full address and at least one category for the business market.")
+        }
+        else {
+            dismissViewControllerAnimated(true, completion: nil)
+        }
         // delegate?.locationController(self, didFinishSelectingLocation: address)
     }
     
@@ -36,4 +104,17 @@ class NewVendorController: UITableViewController {
         super.viewDidLoad()
         
     }
+    
+    func displayAlertWithTitle(title: String, message: String){
+        let controller = UIAlertController(title: title,
+            message: message,
+            preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK",
+            style: .Default,
+            handler: nil)
+        controller.addAction(action)
+        
+        presentViewController(controller, animated: true, completion: nil)
+    }
+
 }
